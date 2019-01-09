4910dbbc32d6caa5e49eb383479f40e08f275a1d
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-01-09 19:43:18 +0330

    1

--- db/migrate/20190102131316_add_uuid_to_categorization.rb
+++
@@ -1,6 +0,0 @@
-class AddUuidToCategorization < ActiveRecord::Migration
-  def change
-    add_column :categorizations, :uuid, :string
-    add_index :categorizations, :uuid, unique: true
-  end
-end
